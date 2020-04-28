fa6be62c3f912c9bb7a1f57eeb43df9bf22aecf9
hk_edu <hk_edu@192.168.2.100>
2020-04-28 10:40:41 +0900

    ~~~~

--- testGitProject2020/src/com/hk/test/Test01.java
+++ testGitProject2020/src/com/hk/test/Test01.java
@@ -5,6 +5,9 @@ public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("saasdfasdf");
 		System.out.println("수정test01");
+		
+		
+		System.out.println("다중수정test");
 	}
 
 }
